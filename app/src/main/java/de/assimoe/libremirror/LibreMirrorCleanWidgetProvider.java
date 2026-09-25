392ffa8fe68a41158fb1f10e14782d5a85b83e1b
AssiMoe <125086721+AssiMoe@users.noreply.github.com>
2026-09-25 18:55:37 +0200

    Add LibreMirror Clean widget provider

---
+++ app/src/main/java/de/assimoe/libremirror/LibreMirrorCleanWidgetProvider.java
@@ -0,0 +1,12 @@
+package de.assimoe.libremirror;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class LibreMirrorCleanWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager manager, int[] ids) {
+        for (int id : ids) WidgetRenderer.update(context, manager, id, WidgetRenderer.STYLE_CLEAN);
+    }
+}
